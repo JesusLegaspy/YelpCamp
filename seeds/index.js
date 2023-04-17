9db4e86f6ae78761b7f44939d32f221301c02633
Jesus Legaspy <jesus.legaspy@gmail.com>
2023-04-17 15:39:41 -0500

    Added styles

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ async function main() {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor((Math.random() * 1000));
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities.at(random1000).city}, ${cities.at(random1000).state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quidem atque, itaque nobis placeat accusamus laboriosam quia, aspernatur voluptates facilis eos et? A nobis delectus non, nihil explicabo id ',
+            price,
         });
         await camp.save();
     }
