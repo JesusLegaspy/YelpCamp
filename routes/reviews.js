320aa762b47f9f028de2396c500a84edac4e5098
Jesus Legaspy <jesus.legaspy@gmail.com>
2023-05-03 15:07:16 -0500

    stars stylesheet and code cleanup

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware.js');
-const Campground = require('../models/campground');
-const Review = require('../models/review.js');
 const reviews = require('../controllers/reviews.js');
-
-const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 
 router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
